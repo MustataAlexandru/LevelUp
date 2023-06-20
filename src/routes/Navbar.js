c5fe111f16f684631d6613aabf8308df7c9eb26a
clk11 <trasca.robert2003@gmail.com>
2023-06-21 00:20:57 +0300

    asd

--- src/routes/Navbar.js
+++ src/routes/Navbar.js
@@ -1,69 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {FiUserCheck} from "react-icons/fi";
-
+import { FiUserCheck } from "react-icons/fi";
+import axios from 'axios';
+import { Spinner } from 'react-bootstrap';
 
 const Navbar = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        let isMounted = true;
+
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('/login', { headers: { 'Content-Type': 'application/json' } });
+
+                if (isMounted) {
+                    setUser(response.data);
+                }
+            } catch (error) {
+                // Handle error here
+                console.error('Error fetching user:', error);
+            }
+        };
+
+        fetchData();
+
+        return () => {
+            isMounted = false;
+        };
+    }, []);
 
     const logoutHandler = () => {
         localStorage.clear();
-        window.location.reload();
+        window.location.href('/');
     }
 
     return (
-        <nav className={styles.navbar}>
-            <h3 className={styles.logo}>Logo</h3>
-            <ul className={styles.nav_links}>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/skills">Skills</Link>
-                </li>
-                <li>
-                    <Link to="/tutorials">Tutorials</Link>
-                </li>
-            </ul>
-            <div className={styles.user_details}>
-                <div className={styles.dropdown}>
-                    <Link
-                        to="#"
-                        className={styles.dropdown_toggle}
-                        role="button"
-                        id="userDropdown"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        <FiUserCheck />
-                    </Link>
-                    <ul className={styles.dropdown_menu} aria-labelledby="userDropdown">
+        <>
+            {user === null ? (
+                <Spinner />
+            ) : (
+                <nav className={styles.navbar}>
+                    <h3 className={styles.logo}>Logo</h3>
+                    <ul className={styles.nav_links}>
                         <li>
-                            <Link className={styles.dropdown_item}>
-                                Name: {}
-                            </Link>
+                            <Link to="/">Home</Link>
                         </li>
                         <li>
-                            <Link className={styles.dropdown_item}>
-                                Email: {}
-                            </Link>
+                            <Link to="/about">About</Link>
                         </li>
                         <li>
-                            <hr />
+                            <Link to="/skills">Skills</Link>
                         </li>
                         <li>
-                            <Link to="/" onClick={logoutHandler} className={styles.dropdown_item}>
-                                Logout
-                            </Link>
+                            <Link to="/tutorials">Tutorials</Link>
                         </li>
                     </ul>
-                </div>
-            </div>
-        </nav>
+                    <div className={styles.user_details}>
+                        <div className={styles.dropdown}>
+                            <Link
+                                to="#"
+                                className={styles.dropdown_toggle}
+                                role="button"
+                                id="userDropdown"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                <FiUserCheck />
+                            </Link>
+                            <ul className={styles.dropdown_menu} aria-labelledby="userDropdown">
+                                <li>
+                                    <span className={styles.dropdown_item}>
+                                        Name: {user.fullname}
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className={styles.dropdown_item}>
+                                        Email: {user.email}
+                                    </span>
+                                </li>
+                                <li>
+                                    <hr />
+                                </li>
+                                <li>
+                                    <Link
+                                        to="/"
+                                        onClick={logoutHandler}
+                                        className={styles.dropdown_item}
+                                    >
+                                        Logout
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            )}
+        </>
     );
 };
 
--- src/server/middlewares/auth.js
+++ src/server/middlewares/auth.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
+const config = require('../config/default.json');
 
 module.exports = (req, res, next) => {
-	const token = req.header('x-auth-token');
-	if (!token) {
-		return res
-			.status(401)
-			.json({ err: 'Access denied , no token provided ...' });
-	}
-	try {
-		const decoded = jwt.verify(token, config.get('jwtSecret'));
-		req.user = decoded.user;
-		next();
-	} catch (err) {
-		res.status(401).json({ err: 'Token is not valid ' });
-	}
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res
+            .status(401)
+            .json({ err: 'Access denied , no token provided ...' });
+    }
+    try {
+        const decoded = jwt.verify(token, config.jwtSecret);
+        req.email = decoded.email;
+        next();
+    } catch (err) {
+        res.status(401).json({ err: 'Token is not valid ' });
+    }
 };
