1e62bb54c39148bea339f774ab4e00e2e1eb1b35
clk11 <74611408+clk11@users.noreply.github.com>
2023-06-28 18:43:33 +0300

    Update db.js

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -1,17 +1,1 @@
-const { Pool } = require('pg');
 
-const context = new Pool({
-	user: 'doadmin',
-	password: 'AVNS_mYpXaAH73810lEN7NP1',
-	host: 'db-postgresql-fra1-26753-do-user-13809431-0.b.db.ondigitalocean.com',
-	port: 25060,
-	database: 'defaultdb',
-	ssl: {
-		rejectUnauthorized: false, 
-	},
-});
-
-
-module.exports = {
-	query: (text, params) => context.query(text, params),
-};
